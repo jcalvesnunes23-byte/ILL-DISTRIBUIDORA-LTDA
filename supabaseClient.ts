a560d83ffe72a417be94e077a26bebe8e76ba891
jcalvesnunes23-byte <jc.alvesnunes23@gmail.com>
2026-03-03 18:11:40 -0300

    fix: atualiza Supabase Anon Key com chave JWT valida

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xsuudajfwlpecsegkntf.supabase.co';
-const supabaseAnonKey = 'sb_publishable_FhSBlvaJc-seW9sz50-U0w_rBTzRDSA';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzdXVkYWpmd2xwZWNzZWdrbnRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NTM3ODQsImV4cCI6MjA4ODEyOTc4NH0.KTBrBlEznN3h_n3S5Uo1oIOqYKrQN3DMJlHVhx2gPEE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
